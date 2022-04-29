2001c175befa609246958dc00a51b52e8f043148
ViktoryiaMakaranka <mvikaf@mail.ru>
2022-04-29 23:50:09 +0300

    config for github

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/nakatere/'
+    : '/',
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
